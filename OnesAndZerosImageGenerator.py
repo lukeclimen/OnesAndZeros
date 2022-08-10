2ae7161b88a4145a6630974cbfc7dddc37663fc3
Luke Climenhage <luke.climenhage@gmail.com>
2022-08-10 10:05:51 -0400

    Tweaking visuals

--- OnesAndZerosImageGenerator.py
+++ OnesAndZerosImageGenerator.py
@@ -1,9 +1,11 @@
 import math
+import random
+
 from PIL import Image, ImageDraw, ImageFont
 
 profile_image = Image.open('Headshot.JPG')
 WIDTH, HEIGHT = profile_image.size
-ONE_ZERO_SIZE = 12
+ONE_ZERO_SIZE = 8
 
 font = ImageFont.truetype('C:/Windows/Fonts/BRITANIC.ttf', ONE_ZERO_SIZE)
 cell_width, cell_height = ONE_ZERO_SIZE, ONE_ZERO_SIZE
@@ -16,23 +18,23 @@ cyber_profile = Image.new('RGB', (WIDTH, HEIGHT), (0, 0, 0))
 d = ImageDraw.Draw(cyber_profile)
 
 for height in range(new_height):
+
     for width in range(new_width):
 
         if width % 2 == 0: continue
+
+        r, g, b = profile_image[width, height]
+        k = int((r + g + b) / 3)
+
+        if g % 50 > 25:
+            g = int(math.ceil(g / 50.0)) * 50
         else:
-
-            r, g, b = profile_image[width, height]
-            k = int((r + g + b) / 3)
-
-            if g % 50 > 25:
-                g = int(math.ceil(g / 50.0)) * 50
-            else:
-                g = int(math.floor(g / 50.0)) * 50
-            if k < 128:
-                text = "1"
-            else:
-                text = "0"
-            d.text((width * cell_width, height * cell_height), text=text, font=font, fill=(0, g, 0))
+            g = int(math.floor(g / 50.0)) * 50
+        if k < 128:
+            text = "1"
+        else:
+            text = "0"
+        d.text((width * cell_width, height * cell_height), text=text, font=font, fill=(0, g, 0))
 
 cyber_profile.show()
 cyber_profile.save('Cyber_Profile.jpg')
