bce8cc0bfd456dd1aaf2f3c89a4aab5ca6fe3e8a
Lucas Climenhage <luke.climenhage@gmail.com>
2024-01-16 21:32:48 -0500

    Improve variable naming
    I am renaming the abismal single-letter variables to something more
    readable, so that if I revisit this repo in another 2 years I won't have
    to figure out what I was doing with it for 10 minutes first.

--- OnesAndZerosImageGenerator.py
+++ OnesAndZerosImageGenerator.py
@@ -4,32 +4,32 @@ import random
 from PIL import Image, ImageDraw, ImageFont
 
 profile_image = Image.open('Headshot.jpg')
-WIDTH, HEIGHT = profile_image.size
+IMAGE_WIDTH, IMAGE_HEIGHT = profile_image.size
 ONE_ZERO_SIZE = 8
 
 font = ImageFont.truetype('Britanic.ttf', ONE_ZERO_SIZE)
 cell_width, cell_height = ONE_ZERO_SIZE, ONE_ZERO_SIZE
 
-profile_image = profile_image.resize((int(WIDTH/cell_width), int(HEIGHT/cell_height)), Image.NEAREST)
+profile_image = profile_image.resize((int(IMAGE_WIDTH/cell_width), int(IMAGE_HEIGHT/cell_height)), Image.NEAREST)
 new_width, new_height = profile_image.size
 profile_image = profile_image.load()
 # Create a new image with the same dimensions of the original, but all black
-cyber_profile = Image.new('RGB', (WIDTH, HEIGHT), (0, 0, 0))
-d = ImageDraw.Draw(cyber_profile)
+cyber_profile = Image.new('RGB', (IMAGE_WIDTH, IMAGE_HEIGHT), (0, 0, 0))
+draw = ImageDraw.Draw(cyber_profile)
 
 for height in range(new_height):
     for width in range(new_width):
-        r, g, b = profile_image[width, height]
-        k = int((r + g + b) / 3)
-        if g % 50 > 25:
-            g = int(math.ceil(g / 50.0)) * 50
+        redValue, greenValue, blueValue = profile_image[width, height]
+        cellAverageIntensity = int((redValue + greenValue + blueValue) / 3)
+        if greenValue % 50 > 25:
+            greenValue = int(math.ceil(greenValue / 50.0)) * 50
         else:
-            g = int(math.floor(g / 50.0)) * 50
-        if k < 128:
+            greenValue = int(math.floor(greenValue / 50.0)) * 50
+        if cellAverageIntensity < 128:
             text = "1"
         else:
             text = "0"
-        d.text((width * cell_width, height * cell_height), text=text, font=font, fill=(0, g, 0))
+        draw.text((width * cell_width, height * cell_height), text=text, font=font, fill=(0, greenValue, 0))
 
 cyber_profile.show()
 cyber_profile.save('Cyber_Profile.jpg')
